d5f9a4c6751ceedbe95d1ff10fa25553356a1f62
Jordan Meier and Jon Nardine <jbirdmeier@gmail.com>
2016-02-18 14:59:32 -0800

    add second spec, where number array is reversed, and add third spec where single digit number outputs a properly worded string

--- tests/NumberWordTest.php
+++ tests/NumberWordTest.php
@@ -18,6 +18,30 @@
             $this->assertEquals(array(1,2,3,4,5), $result);
         }
 
+        function test_reverseArray()
+        {
+            //Arrange
+            $test_NumberWord = new NumberWord;
+            $splitNum = $test_NumberWord->splitNumber(12345);
+            //Act
+            $result = $test_NumberWord->reverseArray($splitNum);
+
+            //Assert
+            $this->assertEquals(array(5,4,3,2,1), $result);
+        }
+
+        function test_convertToWord_singleDigit()
+        {
+            //Arrange
+            $test_NumberWord = new NumberWord;
+            $input = 1;
+            //Act
+            $result = $test_NumberWord->convertToWord($input);
+
+            //Assert
+            $this->assertEquals("one", $result);
+        }
+
 
     }
 
